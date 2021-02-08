cbf7661d63f6a69e26da3f473f8c55c5c96cbf3b
jcosta86 <jeferson.costa@olist.com>
2021-02-08 16:32:07 -0300

    criando metodo update em channels_dao

--- dao/channels_dao.py
+++ dao/channels_dao.py
@@ -5,3 +5,6 @@ from models.channels_model import Channels
 class ChannelsDao(BaseDao):
     def __init__(self):
         super().__init__(Channels)
+
+    def update(self, model: Channels):
+        return super().save(model)
