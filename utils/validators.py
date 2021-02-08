05d325e6b85c664fe8d7c324009412291b1d7112
jcosta86 <jeferson.costa@olist.com>
2021-02-08 13:30:07 -0300

    atualizando requirements e corrigindo bug em validators e channels_model

--- utils/validators.py
+++ utils/validators.py
@@ -6,10 +6,13 @@ def validate_type(key: str, value: object, type: Type) -> object:
         raise TypeError(f"{key.capitalize()} must be {type}!")
     return value
 
+
 def validate_not_empty(key: str, value: str) -> str:
-    if not value.strip():
-        raise ValueError(f"{key.capitalize()} cannot be empty!")
+    if not isinstance(value, float):
+        if not value.strip():
+            raise ValueError(f"{key.capitalize()} cannot be empty!")
     return value
+
 
 def validate_len(key: str, value: object, max_len: int) -> object:
     if len(value) > max_len:
