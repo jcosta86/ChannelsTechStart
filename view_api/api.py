11962d440219eb2250c52be3ed022121c1b191f7
jcosta86 <jeferson.costa@olist.com>
2021-02-10 09:50:04 -0300

    criando api de buyer

--- view_api/api.py
+++ view_api/api.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.channels_resource import ChannelsResource
+from resources.buyer_resource import BuyerResource
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(ChannelsResource, '/api/channels', endpoint='channels')
 api.add_resource(ChannelsResource, '/api/channels/<int:id>', endpoint='channel')
+api.add_resource(BuyerResource, '/api/buyers', endpoint='buyers')
+api.add_resource(BuyerResource, '/api/buyers/<int:id>', endpoint='buyer')
 
 
 @app.route('/')
